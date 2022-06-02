8f0e4a7f70c0dc3ac27a41bee1c0f438b4a5cd53
leeyedam <93389233+leeyedam@users.noreply.github.com>
2022-06-02 20:06:06 +0900

    Update server.js

--- utils/server.js
+++ utils/server.js
@@ -1,3 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://next-ecommerce-front.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://hotel-pi-three.vercel.app/https://hotel-pi-three.vercel.app/';
+
+
+// 'https://next-ecommerce-front.vercel.app'
